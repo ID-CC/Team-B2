09382418d667f716aa85a670ae812919842ed0a1
15615o <kimsr042931@gmail.com>
2022-07-04 22:43:38 +0900

    파일 이름 수정

--- sungryeol/week01/sungryeol.java
+++ sungryeol/week01/sungryeol.java
@@ -1,6 +1,6 @@
 package week01;
 
-public class Hello {
+public class sungryeol {
     public static void main(String[] args) {
         System.out.println("안녕하세요, 소프트웨어융합공학과 1학년 김성렬 입니다. 서울 살고 있고, 부족하지만 잘부탁드립니다!😊");
     }
