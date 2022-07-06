1fa21f0f045328fcd43c0f4d7ca8e05f9005edd0
HeoEunhwa <hsghm3824@naver.com>
2022-07-06 15:57:47 +0900

    Delete minji.cpp

--- minji/week01/minji.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "안녕하세요. IDCC B2 Team STAFF 김민지입니다. 소융과 2학년 재학중이고, 임베디드 SW 개발 5년정도 하고 있습니다. 다른 사람 공부하는거 보는거 좋아합니다 ㅎ.ㅎ 앞으로 잘 부탁드려요 !-!";
-    return 0;
-}
